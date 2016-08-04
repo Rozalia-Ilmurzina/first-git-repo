04d762afe9102735c7c5037d6e87a87d29be1448
Rozalia-Ilmurzina <rozaliailmurzina@outlook.com>
2016-08-04 16:10:56 +0500

    добавила заголовочную библиотеку и добавила вывод Хелло Уорлд

--- test/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ test/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,11 +1,14 @@
 // ConsoleApplication1.cpp: определяет точку входа для консольного приложения.
 //
 
+#include <stdio.h>
 #include "stdafx.h"
-
+#include <iostream>
+using namespace std;
 
 int main()
 {
+	cout << "Hello World";
     return 0;
 }
 
